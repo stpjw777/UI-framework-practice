4c561a901b25165e2e8c3aeb401f0e0f49fa972e
jaewan <stpjw777@gmail.com>
2020-01-28 16:12:11 +0900

    react init

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "@material-ui/core/Button";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import RestoreIcon from "@material-ui/icons/Restore";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import LocationOnIcon from "@material-ui/icons/LocationOn";
+import React from "react";
 
-function App() {
-  const [value, setValue] = useState(0);
-  const onChange = (e, newValue) => {
-    setValue(newValue);
-    console.log(newValue);
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained" color="primary">
-          hello react
-        </Button>
-      </header>
-      <footer>
-        <BottomNavigation value={value} onChange={onChange}>
-          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-        </BottomNavigation>
-      </footer>
-    </div>
+    <>
+      <h1>Hello React</h1>
+    </>
   );
-}
+};
 
 export default App;
