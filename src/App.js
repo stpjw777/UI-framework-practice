e7323e4a5319457c2948bea4513b1095538db885
jaewan <stpjw777@gmail.com>
2020-01-28 16:51:15 +0900

    2020/01/28 bottom Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 
 const App = () => {
+  const [value, setValue] = useState(0);
+
+  const onChange = (e, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
       <h1>Hello React</h1>
+      <BottomNavigation value={value} onChange={onChange} showLabels>
+        <BottomNavigationAction label="Restore" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorite" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Location" icon={<LocationOnIcon />} />
+      </BottomNavigation>
     </>
   );
 };
