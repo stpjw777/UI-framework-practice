77397f0cc0d895108105880f698a98271589039e
jaewan <stpjw777@gmail.com>
2020-01-30 16:42:36 +0900

    review

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 
 const App = () => {
+  const [value, setvalue] = useState(0);
+  const onChange = (e, newValue) => {
+    setvalue(newValue);
+  };
   return (
     <>
       <h1>Hello React</h1>
+      <BottomNavigation value={value} onChange={onChange} showLabels>
+        <BottomNavigationAction label="Restore" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorite" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Location" icon={<LocationOnIcon />} />
+      </BottomNavigation>
     </>
   );
 };
